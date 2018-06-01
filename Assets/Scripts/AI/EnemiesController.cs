1d816f25375a467387ee91af20b2de229dee3122
v.petrov <v.petrov@corpwebgames.com>
2018-06-01 19:29:57 +0300

    Basic enemies spawn system

---
+++ Assets/Scripts/AI/EnemiesController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using Assets.Scripts.Basics;
+using UnityEngine;
+using System.Linq;
+
+namespace Assets.Scripts.AI {
+    class EnemiesController : MbSingleton<EnemiesController> {
+
+        [SerializeField] private GameObject _enemiesHolder;
+
+        [SerializeField] private List<GameObject> _enemiesPrefabs;
+
+        private List<GameObject> _enemies = new List<GameObject>();
+        
+        public List<IDestroyable> Enemies {
+            get {
+                return _enemiesPrefabs.Select(w => w.GetComponent<IDestroyable>()).ToList();
+            }
+        }
+
+        private float timeToSpawn = 0;
+        private float spawnCooldown = 3;
+
+
+        private void Start() {
+            
+        }
+
+
+        private void Update() {
+
+            if (timeToSpawn <= 0 && _enemies.Count < 10) {
+                var enemy = SpawnEnemy();
+                timeToSpawn = spawnCooldown;
+                return;
+            }
+
+            timeToSpawn -= Time.deltaTime;
+        }
+
+        private GameObject SpawnEnemy() {
+            var spawnPos = new Vector3(Random.Range(2, 15), 2, 0);
+            var spawnRot = Quaternion.identity;
+            var enemy = Instantiate(_enemiesPrefabs[0], spawnPos, spawnRot, _enemiesHolder.transform);
+            return enemy;
+        }
+
+
+    }
+}
+
+
