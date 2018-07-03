5ea4c5caa2ea1ac6c70c85d413bcee5878af7fe1
v.petrov <v.petrov@corpwebgames.com>
2018-07-03 10:37:51 +0300

    More physics updates. (Full rigidbody)

--- Assets/Scripts/Vehicles/Handling/PlayerHandling/FullRigidBodyCarPhysics.cs
+++ Assets/Scripts/Vehicles/Handling/PlayerHandling/FullRigidBodyCarPhysics.cs
@@ -1,18 +1,17 @@
 ﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 using System;
-using System.Globalization;
 using System.Linq;
-
 using Assets.Scripts.Vehicles.Machines;
-
 using Tools;
 using UnityEngine;
 using UnityEngine.UI;
 
 // TODO: Separate this to "phisics" class
 public class FullRigidBodyCarPhysics : IHandlingBehaviour
-{    
+{
+    private float _h, _v;
+
     public HandlingCondition CurrentCondition { get; set; }
     public GameObject HandlingObject { get; set; }
 
@@ -40,24 +39,24 @@ public class FullRigidBodyCarPhysics : IHandlingBehaviour
     }
 
     public void Update() {
-        float h = -Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
 
-        var acceleration = _currentVehicle.Acceleration;
+        ProcessInput();
 
+        ProcessPhysics();
+    }
+
+    private void ProcessPhysics() {
         var steering = _currentVehicle.Steering;
 
-        Vector2 speed = HandlingObject.transform.up * (v * acceleration);
+        Vector2 speed = HandlingObject.transform.up * (_v * _currentVehicle.Acceleration);
         _vehicleRb.AddForce(speed);
 
         float direction = Vector2.Dot(_vehicleRb.velocity, _vehicleRb.GetRelativeVector(Vector2.up));
         if (direction >= 0.0f) {
-            //_vehicleRb.rotation += h * steering * (_vehicleRb.velocity.magnitude / 5.0f);
-            _vehicleRb.AddTorque((h * steering) * (_vehicleRb.velocity.magnitude / 10.0f));
+            _vehicleRb.AddTorque((_h * steering) * (_vehicleRb.velocity.magnitude / 10.0f));
         }
         else {
-            //_vehicleRb.rotation -= h * steering * (_vehicleRb.velocity.magnitude / 5.0f);
-            _vehicleRb.AddTorque((-h * steering) * (_vehicleRb.velocity.magnitude / 10.0f));
+            _vehicleRb.AddTorque((-_h * steering) * (_vehicleRb.velocity.magnitude / 10.0f));
         }
 
         Vector2 forward = new Vector2(0.0f, 0.5f);
@@ -70,43 +69,32 @@ public class FullRigidBodyCarPhysics : IHandlingBehaviour
         }
 
         Vector2 rightAngleFromForward = Quaternion.AngleAxis(steeringRightAngle, Vector3.forward) * forward;
+        Debug.Log("Rught angle from forward: " + rightAngleFromForward);
+
         Debug.DrawLine((Vector3)_vehicleRb.position, (Vector3)_vehicleRb.GetRelativePoint(rightAngleFromForward), Color.green);
-
         float driftForce = Vector2.Dot(_vehicleRb.velocity, _vehicleRb.GetRelativeVector(rightAngleFromForward.normalized));
-
         Vector2 relativeForce = (rightAngleFromForward.normalized * -1.0f) * (driftForce * 10.0f);
-
-
         Debug.DrawLine((Vector3)_vehicleRb.position, (Vector3)_vehicleRb.GetRelativePoint(relativeForce), Color.red);
 
         _vehicleRb.AddForce(_vehicleRb.GetRelativeVector(relativeForce));
     }
 
-    //public Vector3 CurrentVelocity { get; set; }
-    //public HandlingCondition CurrentCondition { get; set; }
-    //public GameObject HandlingObject { get; set; }
+    private void ProcessInput() {
+        _v = 0.5f + Input.GetAxis("Vertical") / 1.5f;
+        
+        var userHandlingPos = Camera.main.ScreenToWorldPoint(InputTool.InputPosition).x - HandlingObject.transform.position.x;
+        _h = - Mathf.Clamp(userHandlingPos, -1, 1);
 
-    //private readonly Text _speedText;
-    //private readonly VehicleBase _currentVehicle;
-    //private Vector3 _prevPosition;
-
-    //public PlayerCarHandling2(GameObject player, VehicleBase currentVehicle)
-    //{
-    //	HandlingObject = player;
-    //	_currentVehicle = currentVehicle;
-    //	_prevPosition = HandlingObject.transform.position;
-    //	SetSpecifications();
-
-    //	var uiCanvas = GameObject.Find("UiCanvas").GetComponent<Canvas>();
-    //	_speedText = uiCanvas.transform.Find("SpeedLabel").GetComponent<Text>();
-    //	CurrentCondition = HandlingCondition.OnGround;
-    //}
-
-    //public void Update()
-    //{
-    //	UpdateHandling();
-    //	UiUpdate();
-    //}
+        if (_h == 0) {
+            var curAngle = HandlingObject.transform.rotation.eulerAngles.z;
+            if (curAngle >= 1 && curAngle <= 180) { // Turned left
+                _h = 0.5f;
+            }
+            else if (curAngle > 180 && curAngle <= 359) { // Turned right
+                _h = -0.5f;
+            }
+        }
+    }
 
     public void InstallEquipment() {
         foreach (var eqSlot in _currentVehicle.Slots.OfType<EquipmentSlot>()) {
@@ -116,66 +104,4 @@ public class FullRigidBodyCarPhysics : IHandlingBehaviour
         SetSpecifications();
     }
 
-    //public void Restart()
-    //{
-    //	HandlingObject.transform.position = Vector3.zero;
-    //}
-
-    //private void SetSpecifications()
-    //{
-    //	HandlingObject.GetComponent<Rigidbody2D>().mass 
-    //		= _currentVehicle.Mass;
-    //}
-
-    //private void UpdateHandling()
-    //{
-    //	// ACCELERATING
-    //	var movingDelta = HandlingObject.transform.position
-    //					  - _prevPosition;
-    //	CurrentVelocity = movingDelta / Time.deltaTime;
-    //	var currentSpeed = Mathf.Clamp(CurrentVelocity.y, 0, _currentVehicle.MaxSpeed);
-    //	var maxSpeed = _currentVehicle.MaxSpeed;
-    //	currentSpeed += 1 - currentSpeed / maxSpeed;
-    //	_prevPosition = HandlingObject.transform.position;
-    //	HandlingObject.transform.Translate(Vector2.up * currentSpeed * Time.deltaTime);
-
-    //	// STEERING
-    //	var userHandlingPos = Camera.main.ScreenToWorldPoint(InputTool.InputPosition);
-    //	HandlingObject.transform.position =
-    //		Vector2.MoveTowards(
-    //			HandlingObject.transform.position,
-    //			new Vector2(
-    //				userHandlingPos.x,
-    //				HandlingObject.transform.position.y),
-    //			_currentVehicle.Steering * Time.deltaTime);
-
-    //	// ALIGNING
-    //	var playerRotation =
-    //		HandlingObject.transform.rotation.eulerAngles.z;
-    //	if (playerRotation > 0.5f && playerRotation < 359.5f)
-    //	{
-    //		var rotateAngle = _currentVehicle.Steering * 15
-    //		                  * Time.deltaTime;
-
-    //		if (playerRotation < 180) rotateAngle *= -1;
-
-    //		HandlingObject.transform.Rotate(new Vector3(0, 0, rotateAngle));
-
-    //		if (Math.Abs(playerRotation - HandlingObject.transform.rotation.eulerAngles.z) > 180)
-    //		{
-    //			HandlingObject.transform.rotation = Quaternion.identity;
-    //		}
-    //	}
-    //	else
-    //	{
-    //		HandlingObject.transform.rotation = Quaternion.identity;
-    //	}
-    //}
-
-    //// TODO: Extract to separate class
-    //private void UiUpdate()
-    //{
-    //	var showingSpeed = Mathf.Round(CurrentVelocity.y * 10);
-    //	_speedText.text = showingSpeed.ToString(CultureInfo.InvariantCulture);
-    //}
-}
+   }
